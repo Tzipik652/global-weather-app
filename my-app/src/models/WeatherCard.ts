b0d1e46ca696fdc240e5a0ffff8c9e878516e35a
Tzipik652 <tzipik652@gmail.com>
2025-06-05 18:16:37 +0300

    feat(model): add WeatherCardType model for typed weather data

---
+++ my-app/src/models/WeatherCard.ts
@@ -0,0 +1,14 @@
+export interface WeatherCardModel {
+  city: string;
+  season: string;
+  humidity: number;
+  realFeel: number;
+  temperature: number;
+}
+export interface  WeatherCardType  {
+  city: string;
+  season: string;
+  humidity: number;
+  realFeel: number;
+  temperature: number;
+}
